bad8d792e757f4be6dc344f1a5ddae4913ead2ff
Ignacio R Espino <irespino@capozios.com>
2018-08-28 20:00:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(curr_line)
+  if curr_line.count == 0 
+    puts "The line is currently empty."
+  else
+    string = 'The line is currently:'
+    curr_line.each_with_index do |name, index|
+      string += " #{index + 1}. #{name}"
+    end
+    puts string
+  end
+end
+
+def take_a_number(curr_line, name)
+  
+end
